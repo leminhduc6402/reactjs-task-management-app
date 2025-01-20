ebb9257f9d18e692239835adfc0ae089513f31aa
leminhduc6402 <leminhduc6402@gmail.com>
2025-01-20 20:00:37 +0700

    Design simple Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,54 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LayoutApp from "./LayoutApp";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <LayoutApp />,
+      children: [
+        {
+          path: "/",
+          element: <div>Hello World</div>,
+        },
+        {
+          path: "/timeline",
+          element: <div>Timeline</div>,
+        },
+        {
+          path: "/search",
+          element: <div>Search</div>,
+        },
+        {
+          path: "/home",
+          element: <div>Home</div>,
+        },
+        {
+          path: "/settings",
+          element: <div>Settings</div>,
+        },
+        {
+          path: "/users",
+          element: <div>Users</div>,
+        },
+        {
+          path: "/teams",
+          element: <div>Team</div>,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <LayoutApp />,
-    children: [
-      {
-        path: "/",
-        element: <div>Hello World</div>,
-      },
-    ],
-  },
-]);
+    future: {
+      v7_relativeSplatPath: true,
+    },
+  }
+);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <RouterProvider router={router} future={{ v7_startTransition: true }} />
+  );
 }
 
 export default App;
